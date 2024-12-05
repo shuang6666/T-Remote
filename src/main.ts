0b36462e22b256c4fb07fbdd69a5248c2437cd63
Yishuang Tian <yishuang.tian@sap.com>
2024-12-05 08:19:23 +0000

    Add Element Plus

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app').$nextTick(() => {
+createApp(App).use(ElementPlus).mount('#app').$nextTick(() => {
   // Use contextBridge
   window.ipcRenderer.on('main-process-message', (_event, message) => {
     console.log(message)
